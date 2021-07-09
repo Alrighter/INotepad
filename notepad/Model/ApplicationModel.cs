ed8097680ccb28aa8f349cfb0b5476d1b8cb174f
Alrighter <59889888+Alrighter@users.noreply.github.com>
2021-07-09 20:23:25 +0300

    Add project files.

---
+++ notepad/Model/ApplicationModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows;
+
+namespace notepad.Model
+{
+    class ApplicationModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Text { get; set; }
+        public string FileName { get; set; } = string.Empty;
+        public string Path { get; set; }
+
+        public void SaveFile(bool SaveAs = false)
+        {
+            SaveFileDialog saveFileDialog = new() { Filter = "txt|*.txt", FileName = FileName };
+            if (SaveAs || FileName == "New_File")
+            {
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    File.WriteAllText(saveFileDialog.FileName, Text);
+                    Path = saveFileDialog.FileName;
+                }
+            }
+            else if (SaveAs == false)
+            {
+                try
+                {
+                    File.WriteAllText(Path, Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Save or open file firstly.");
+                }
+                
+            }
+        }
+
+        public void NewFile(bool NewFile = false)
+        {
+            SaveFileDialog saveFileDialog = new() { Filter = "txt|*.txt", FileName = FileName };
+            if (NewFile)
+            {
+                saveFileDialog.ShowDialog();
+                File.Create(saveFileDialog.FileName);
+                Path = saveFileDialog.FileName;
+            }
+            
+        }
+        
+        public void OpenFile(bool OpenFile = false)
+        {
+            OpenFileDialog openFileDialog = new() { Filter = "txt|*.txt" };
+            if (OpenFile)
+            {
+                openFileDialog.ShowDialog();
+                Path = openFileDialog.FileName;
+                Text = File.OpenText(Path).ReadLine();
+            }
+        }
+
+    }
+}
---
+++ notepad/ViewModel/ApplicationViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace notepad.ViewModel
+{
+    class ApplicationViewModel : INotifyPropertyChanged
+    {
+
+        public Model.ApplicationModel Model { get; set; } = new();
+        public event PropertyChangedEventHandler PropertyChanged;
+        public RelayCommand SaveAs => new(obj => Model.SaveFile(true));
+        public RelayCommand Save => new(obj => Model.SaveFile(false));
+        public RelayCommand NewFile => new(obj => Model.NewFile(true));
+        public RelayCommand OpenFile => new(obj => Model.OpenFile(true));
+
+    }
+}
